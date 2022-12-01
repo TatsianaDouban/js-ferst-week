e781a03a239df78162d8dc09635c8f6e47b35b0d
tatsiana <Tatsiana3345260@gmail.com>
2022-12-01 15:56:33 +0300

    new c

---
+++ index.js
@@ -0,0 +1,43 @@
+
+let name;
+function yourname() {
+    name = prompt("Введите имя");
+    alert (`Привет ${name}`);
+}
+
+ let x 
+ let y
+ let solver
+
+function sum(){
+    x = prompt("введите число")
+    y = prompt ("введите второе число")
+    solver = Number(x) + Number(y);
+    alert(`равно ${solver}`)
+}  
+
+function minus(){
+    x = prompt("введите число")
+    y = prompt ("введите второе число")
+    solver = x-y;
+    alert(`равно ${solver}`)
+}
+
+function divitiom(){
+    x = prompt("введите число")
+    y = prompt ("введите второе число")
+    solver = x/y;
+    alert(`равно ${solver}`)
+}
+
+function myltiply(){
+    x = prompt("введите число")
+    y = prompt ("введите второе число")
+    solver = x*y;
+    alert(`равно ${solver}`)
+}
+
+
+ 
+
+
